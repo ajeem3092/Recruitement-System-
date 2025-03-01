b7b3ba39261574f5a2708cc51390af5de54ca478
Ajeem Makandar <123727338+ajeem3092@users.noreply.github.com>
2025-03-01 19:47:47 +0530

    Add files via upload

---
+++ candidate application form/.next/static/chunks/app_page_5a41f9c9.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_5a41f9c9.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_5a41f9c9.js",
+  "chunks": [
+    "static/chunks/_4e9d1fac._.js",
+    "static/chunks/app_styles_module_4bb65298.css"
+  ],
+  "source": "dynamic"
+});
